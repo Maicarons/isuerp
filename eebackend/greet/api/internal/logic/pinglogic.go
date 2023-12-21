08f4803b5ec94cd0141f3cd723517ff0ec868614
Maicarons <136282328+Maicarons@users.noreply.github.com>
2023-12-21 18:33:50 +0800

    create 142 files

---
+++ eebackend/greet/api/internal/logic/pinglogic.go
@@ -0,0 +1,31 @@
+package logic
+
+import (
+	"context"
+
+	"github.com/zeromicro/go-zero/core/logx"
+
+	"api/internal/svc"
+	"api/internal/types"
+)
+
+type PingLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewPingLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PingLogic {
+	return &PingLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *PingLogic) Ping() (resp *types.Resp, err error) {
+	resp = new(types.Resp)
+	resp.Msg = "pong"
+
+	return
+}
---
+++ eebackend/greet/api/internal/svc/servicecontext.go
@@ -0,0 +1,17 @@
+package svc
+
+import (
+	"api/internal/config"
+)
+
+type ServiceContext struct {
+	Config   config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	
+	return &ServiceContext{
+		Config:   c,
+		
+	}
+}
